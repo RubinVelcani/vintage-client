147fe2288a9ddc6cd39a9f740d2f5d36f4fcd572
RubinVelcani <rubin.velcani@gmail.com>
2021-07-12 10:04:06 +0200

    build fix commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import CartPage from './pages/CartPage';
+import HomePage from './pages/HomePage';
+import OrderHistoryPage from './pages/OrderHistoryPage';
+import OrderPage from './pages/OrderPage';
+import PaymentMethodPage from './pages/PaymentMethodPage';
+import PlaceOrderPage from './pages/PlaceOrderPage';
+import ProductPage from './pages/ProductPage';
+import ProfilePage from './pages/ProfilePage';
+import RegisterPage from './pages/RegisterPage';
+import ShippingAddressPage from './pages/ShippingAddressPage';
+import SignInPage from './pages/SignInPage';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  }
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <Link className="brand" to="/">VintAge</Link>
+          <div>
+            <Link to="/cart">Cart
+              {cartItems.length > 0 && (
+                <span className="badge">
+                  {cartItems.length}
+                </span>
+              )}
+            </Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">{userInfo.name}<i className="fa fa-caret-down"></i></Link>
+                  <ul className="dropdown-content">
+                    <li>
+                      <Link to="/profile">User Profile</Link>
+                    </li>
+                    <li>
+                      <Link to="/orderhistory">Order History</Link>
+                    </li>
+                    <li>
+                      <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+                    </li>
+                  </ul>
+                </div>
+              ) :
+                (
+                  <Link to="/signin">Sign In</Link>
+                )
+            }
+            {
+              userInfo && userInfo.isAdmin && (
+                <div className="dropdown">
+                  <Link to="#admin">Admin {' '} <i className="fa fa-caret-down"></i></Link>
+                  <ul className="dropdown-content">
+                    <li>
+                      <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                      <Link to="/projectlist">Products</Link>
+                    </li>
+                    <li>
+                      <Link to="/orderlist">Orders</Link>
+                    </li>
+                    <li>
+                      <Link to="/userList">Users</Link>
+                    </li>
+                  </ul>
+                </div>
+              )
+            }
+          </div>
+        </header>
+        <main>
+          <Route path='/cart/:id?' component={CartPage} />
+          <Route path='/products/:id' component={ProductPage} />
+          <Route path='/register' component={RegisterPage} />
+          <Route path='/signin' component={SignInPage} />
+          <Route path='/shipping' component={ShippingAddressPage} />
+          <Route path='/payment' component={PaymentMethodPage} />
+          <Route path='/placeorder' component={PlaceOrderPage} />
+          <Route path='/order/:id' component={OrderPage} />
+          <Route path='/orderhistory' component={OrderHistoryPage} />
+          <PrivateRoute path='/profile' component={ProfilePage} />
+          <Route path='/' component={HomePage} exact />
+        </main>
+        <footer className="row center">
+          All rights reserved
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Trousers',
+      category: 'pants',
+      image: '/images/p6.jpg',
+      price: 79,
+      countInStock: 15,
+      brand: 'Dolce Gabbana',
+      rating: 4.5,
+      numReviews: 14,
+      desctiption: 'Checkerd stylisht trousers for the office'
+    },
+    {
+      _id: '2',
+      name: 'Slim Trousers',
+      category: 'pants',
+      image: '/images/p6.jpg',
+      price: 79,
+      countInStock: 56,
+      brand: 'Dolce Gabbana',
+      rating: 4.5,
+      numReviews: 14,
+      desctiption: 'Checkerd stylisht trousers for the office'
+    },
+    {
+      _id: '3',
+      name: 'Slim Trousers',
+      category: 'pants',
+      image: '/images/p6.jpg',
+      price: 79,
+      countInStock: 34,
+      brand: 'Dolce Gabbana',
+      rating: 4.5,
+      numReviews: 14,
+      desctiption: 'Checkerd stylisht trousers for the office'
+    },
+    {
+      _id: '4',
+      name: 'Slim Trousers',
+      category: 'pants',
+      image: '/images/p6.jpg',
+      price: 79,
+      countInStock: 0,
+      brand: 'Dolce Gabbana',
+      rating: 4.5,
+      numReviews: 14,
+      desctiption: 'Checkerd stylisht trousers for the office'
+    },
+    {
+      _id: '5',
+      name: 'Slim Trousers',
+      category: 'pants',
+      image: '/images/p6.jpg',
+      price: 79,
+      countInStock: 56,
+      brand: 'Dolce Gabbana',
+      rating: 4.5,
+      numReviews: 14,
+      desctiption: 'Checkerd stylisht trousers for the office'
+    },
+    {
+      _id: '6',
+      name: 'Slim Trousers',
+      category: 'pants',
+      image: '/images/p6.jpg',
+      price: 79,
+      countInStock: 23,
+      brand: 'Dolce Gabbana',
+      rating: 4.5,
+      numReviews: 14,
+      desctiption: 'Checkerd stylisht trousers for the office'
+    },
+  ]
+}
+
+export default data;
